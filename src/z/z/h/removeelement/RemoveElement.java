28fae0894dc647f6859526e063bfcd86bd128690
zzhihua <zengzhihua@trkj.cn>
2021-07-08 16:05:45 +0800

    27. 移除元素

---
+++ src/z/z/h/removeelement/RemoveElement.java
@@ -0,0 +1,47 @@
+package z.z.h.removeelement;
+
+/**
+ * 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+ * <p>
+ * 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+ * <p>
+ * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/remove-element
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author zzhihua
+ * @date 2021/7/8
+ */
+public class RemoveElement {
+
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 2, 3};
+        int i = removeElement(nums, 3);
+        System.out.println(i);
+    }
+
+    /**
+     * 同27题思路一致，采用双指针思路
+     *
+     * @param nums
+     * @param val
+     * @return
+     */
+    public static int removeElement(int[] nums, int val) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int fast = 0;
+        int slow = 0;
+        while (fast < nums.length) {
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        return slow;
+    }
+}
