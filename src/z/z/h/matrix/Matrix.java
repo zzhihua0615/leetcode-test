6d9ebac31837d9e260f0f647e409d3be2aa0c026
zzhihua <zengzhihua@trkj.cn>
2021-12-22 15:43:38 +0800

    48. 旋转图像

---
+++ src/z/z/h/matrix/Matrix.java
@@ -0,0 +1,36 @@
+package z.z.h.matrix;
+
+/**
+ * @author zzhihua
+ * @date 2021/12/22
+ */
+public class Matrix {
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[3][3];
+        matrix[0] = new int[]{1, 2, 3};
+        matrix[1] = new int[]{4, 5, 6};
+        matrix[2] = new int[]{7, 8, 9};
+        rotate(matrix);
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println(matrix);
+    }
+
+    public static void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = 0; j < (n + 1) / 2; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = temp;
+            }
+        }
+    }
+}
